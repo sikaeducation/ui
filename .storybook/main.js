1909dd2077580cd136b005c513643673689f00d8
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-20 15:37:46 -0600

    Updating storybook formats

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,32 @@
-import remarkGfm from "remark-gfm"
+// import remarkGfm from "remark-gfm"
 
 export default {
-	stories: [
+    stories: [
 		"../src/**/*.mdx",
 		"../src/**/*.stories.mdx",
 		"../src/**/*.stories.@(js|jsx|ts|tsx)"
 	],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"@storybook/preset-create-react-app",
-		"storybook-addon-react-router-v6",
-		"@storybook/addon-a11y",
-		{
-			name: "@storybook/addon-docs",
-			options: {
-				mdxPluginOptions: {
-					mdxCompilationOptions: {
-						remarkPlugins: [remarkGfm],
-					}
-				}
-			},
-		},
-	],
-	framework: "@storybook/react",
-	core: {
-		builder: "@storybook/builder-webpack5",
+
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/preset-create-react-app",
+        "storybook-addon-react-router-v6",
+        "@storybook/addon-a11y",
+        "@storybook/addon-mdx-gfm"
+    ],
+
+    framework: {
+		name: "@storybook/react-webpack5",
+		options: {}
 	},
-	features: {
+
+    features: {
 		interactionsDebugger: true,
 	},
+
+    docs: {
+        autodocs: true
+    }
 };
--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -7,43 +7,43 @@ import Icon from "../Icon";
 import "./Drawer.scss";
 
 type Props = {
-  children: ReactNode;
-  close: () => void;
-  modal?: boolean;
+	children: ReactNode;
+	close: () => void;
+	modal?: boolean;
 };
 
 export default function Drawer({ children, close, modal = false }: Props) {
-  const handleClick = () => {
-    if (modal) {
-      close();
-    }
-  };
-  const handleKey = (event: unknown) => {
-    if (event instanceof KeyboardEvent && event.code === "Escape") {
-      close();
-    }
-  };
-  return (
-    <div
-      role="dialog"
-      aria-modal={modal}
-      aria-describedby="drawer-content"
-      aria-label={`${modal && "modal "}drawer`}
-      className={classNames({ Drawer: true, modal })}
-      onClick={handleClick}
-      onKeyDown={handleKey}
-    >
-      <div className="content" id="drawer-content">
-        <button
-          className="close-button"
-          type="button"
-          autoFocus
-          onClick={() => close()}
-        >
-          <Icon type="close" />
-        </button>
-        {children}
-      </div>
-    </div>
-  );
+	const handleClick = () => {
+		if (modal) {
+			close();
+		}
+	};
+	const handleKey = (event: unknown) => {
+		if (event instanceof KeyboardEvent && event.code === "Escape") {
+			close();
+		}
+	};
+	return (
+		<div
+			role="dialog"
+			aria-modal={modal}
+			aria-describedby="drawer-content"
+			aria-label={`${modal && "modal "}drawer`}
+			className={classNames({ Drawer: true, modal })}
+			onClick={handleClick}
+			onKeyDown={handleKey}
+		>
+			<div className="content" id="drawer-content">
+				<button
+					className="close-button"
+					type="button"
+					autoFocus
+					onClick={() => close()}
+				>
+					<Icon type="close" />
+				</button>
+				{children}
+			</div>
+		</div>
+	);
 }
