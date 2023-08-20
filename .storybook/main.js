f6e2b804bae08e833ba1fef9270eaeaae880063f
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-20 15:41:19 -0600

    Update main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,26 @@
-// import remarkGfm from "remark-gfm"
-
 export default {
-    stories: [
+	stories: [
 		"../src/**/*.mdx",
 		"../src/**/*.stories.mdx",
 		"../src/**/*.stories.@(js|jsx|ts|tsx)"
 	],
-
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/preset-create-react-app",
-        "storybook-addon-react-router-v6",
-        "@storybook/addon-a11y",
-        "@storybook/addon-mdx-gfm"
-    ],
-
-    framework: {
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/preset-create-react-app",
+		"storybook-addon-react-router-v6",
+		"@storybook/addon-a11y",
+		"@storybook/addon-mdx-gfm"
+	],
+	framework: {
 		name: "@storybook/react-webpack5",
 		options: {}
 	},
-
-    features: {
+	features: {
 		interactionsDebugger: true,
 	},
-
-    docs: {
-        autodocs: true
-    }
+	docs: {
+		autodocs: true
+	}
 };
