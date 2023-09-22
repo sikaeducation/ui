82fc7b1926e6442f412edccde0e98edceedb8673
Kyle Coberly <kyle.coberly@gmail.com>
2023-09-22 14:10:06 -0600

    Create text content style

---
+++ src/elements/TextContent/TextContent.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import "./TextContent.scss";
+import classNames from "classnames";
+
+type Props = {
+	children: ReactNode;
+	className: string;
+};
+
+export default function TextArea({
+	children,
+	className,
+}: Props) {
+	return (
+		<p className={classNames({
+			[className]: true,
+			TextContent: true,
+		})}>
+			{children}
+		</p>
+	);
+}
