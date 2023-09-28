70186113d8a37faff0ca49e2bd8d8aa399f225cd
Kyle Coberly <kyle.coberly@gmail.com>
2023-09-27 20:29:24 -0600

    Refactor form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,7 @@
-import { BaseFormControl, FormControl } from "./form-controls";
+import { BaseFormControl, FormControl, getFormControl } from "./form-controls";
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
 import Button from "../../elements/Button";
 import Heading from "../../elements/Heading";
-import { controlTypes } from "./form-controls";
 import "./Form.scss";
 
 type FormData = string | boolean | number | string[];
@@ -31,71 +30,9 @@ export default function Form({
 			<Heading level={2}>{heading}</Heading>
 			<form>
 				{fields.map((field) => {
-					const { id, label, controlType } = field;
-					const commonOptions = {
-						id: id,
-						label: label,
-						value: newItem[String(id)] ? String(newItem[String(id)]) : "",
-						updateValue: (newValue: FormData) => setNewItem({
-							...newItem,
-							[id]: newValue,
-						}),
-					};
-					if (controlType === "DropDown") {
-						return controlTypes[controlType](
-							field, newItem, setNewItem,
-						);
-					}
-					if (controlType === "TextInput") {
-						return controlTypes[controlType](
-							field, newItem, setNewItem,
-						);
-					}
-					if (controlType === "TextArea") {
-						return controlTypes[controlType]({
-							...commonOptions,
-							key: id,
-						});
-					}
-					if (controlType === "Checkbox") {
-						return controlTypes[controlType]({
-							...commonOptions,
-							key: id,
-							type: field.type,
-						});
-					}
-					if (controlType === "Toggle") {
-						return controlTypes[controlType]({
-							...commonOptions,
-							key: id,
-						});
-					}
-					if (controlType === "TagManager") {
-						const tags = newItem[id];
-						if (!Array.isArray(tags)) return <></>;
-						return controlTypes[controlType]({
-							...commonOptions,
-							key: id,
-							tags: tags,
-							removeTag: (tagToRemove: string) => {
-								setNewItem({
-									...newItem,
-									[id]: tags
-										.filter((tag) => tag !== tagToRemove),
-								});
-							},
-							addTag: (tag: string) => {
-								setNewItem({
-									...newItem,
-									[id]: [
-										...tags,
-										"hi",
-										tag,
-									],
-								});
-							},
-						});
-					}
+					return getFormControl(
+						field, newItem, setNewItem,
+					);
 				})}
 				{children}
 				<fieldset className="actions">
--- src/components/Form/form-controls.tsx
+++ src/components/Form/form-controls.tsx
@@ -53,12 +53,32 @@ export type FormControl = | FormControlTextInput
 	| FormControlTagManager
 	| FormControlToggle;
 
-export const controlTypes = {
+const isTextInput = (field: FormControl): field is FormControlTextInput => {
+	return field.controlType === "TextInput";
+};
+const isTextArea = (field: FormControl): field is FormControlTextArea => {
+	return field.controlType === "TextArea";
+};
+const isDropDown = (field: FormControl): field is FormControlDropDown => {
+	return field.controlType === "DropDown";
+};
+const isCheckbox = (field: FormControl): field is FormControlCheckbox => {
+	return field.controlType === "Checkbox";
+};
+const isTagManager = (field: FormControl): field is FormControlTagManager => {
+	return field.controlType === "TagManager";
+};
+const isToggle = (field: FormControl): field is FormControlToggle => {
+	return field.controlType === "Toggle";
+};
+
+const controlTypes = {
 	"DropDown": (
-		field: FormControlDropDown,
+		field: FormControl,
 		newItem: Record<string, FormData>,
 		setNewItem: (newItem: Record<string, FormData>) => void,
 	) => {
+		if (!isDropDown(field)) return <></>;
 		const { id, label } = field;
 		const value = newItem[String(id)] ? String(newItem[String(id)]) : "";
 		const updateValue = (newValue: FormData) => {
@@ -82,6 +102,7 @@ export const controlTypes = {
 		newItem: Record<string, FormData>,
 		setNewItem: (newItem: Record<string, FormData>) => void,
 	) => {
+		if (!isTextInput(field)) return <></>;
 		const { id, label } = field;
 		const value = newItem[String(id)] ? String(newItem[String(id)]) : "";
 		const updateValue = (newValue: FormData) => {
@@ -100,9 +121,146 @@ export const controlTypes = {
 			updateValue={updateValue}
 		/>;
 	},
-	// Next: Do the above stuff to the below types
-	"TextArea": (props) => <TextArea {...props} />,
-	"Checkbox": (props) => <Checkbox {...props} />,
-	"Toggle": (props) => <Toggle {...props} />,
-	"TagManager": (props) => <TagManager {...props} />,
-};
+	"TextArea": (
+		field: FormControlTextArea,
+		newItem: Record<string, FormData>,
+		setNewItem: (newItem: Record<string, FormData>) => void,
+	) => {
+		if (!isTextArea(field)) return <></>;
+		const { id, label } = field;
+		const value = newItem[String(id)] ? String(newItem[String(id)]) : "";
+		const updateValue = (newValue: FormData) => {
+			return setNewItem({
+				...newItem,
+				[id]: newValue,
+			});
+		};
+		return <TextArea
+			key={id}
+			id={id}
+			label={label}
+			value={value}
+			updateValue={updateValue}
+		/>;
+	},
+	"Checkbox": (
+		field: FormControlCheckbox,
+		newItem: Record<string, FormData>,
+		setNewItem: (newItem: Record<string, FormData>) => void,
+	) => {
+		if (!isCheckbox(field)) return <></>;
+		const { id, label, type } = field;
+		const value = newItem[String(id)] ? String(newItem[String(id)]) : "";
+		const updateValue = (newValue: FormData) => {
+			return setNewItem({
+				...newItem,
+				[id]: newValue,
+			});
+		};
+		return <Checkbox
+			key={id}
+			id={id}
+			label={label}
+			type={type}
+			value={value}
+			updateValue={updateValue}
+		/>;
+	},
+	"Toggle": (
+		field: FormControlToggle,
+		newItem: Record<string, FormData>,
+		setNewItem: (newItem: Record<string, FormData>) => void,
+	) => {
+		if (!isToggle(field)) return <></>;
+		const { id, label } = field;
+		const value = newItem[String(id)] ? String(newItem[String(id)]) : "";
+		const updateValue = (newValue: FormData) => {
+			return setNewItem({
+				...newItem,
+				[id]: newValue,
+			});
+		};
+		return <Toggle
+			key={id}
+			id={id}
+			label={label}
+			value={value}
+			updateValue={updateValue}
+		/>;
+	},
+	"TagManager": (
+		field: FormControlTagManager,
+		newItem: Record<string, FormData>,
+		setNewItem: (newItem: Record<string, FormData>) => void,
+	) => {
+		if (!isTagManager(field)) return <></>;
+		const { id } = field;
+
+		let tags: string[];
+		if (isStringArray(newItem[id])) {
+			tags = newItem[id] as string[];
+		} else return <></>;
+
+		if (!Array.isArray(tags)) return <></>;
+		return <TagManager
+			key={id}
+			id={id}
+			tags={tags}
+			addTag={(tag: string) => {
+				setNewItem({
+					...newItem,
+					[id]: [
+						...tags,
+						"hi",
+						tag,
+					],
+				});
+			}}
+			removeTag={(tagToRemove: string) => {
+				setNewItem({
+					...newItem,
+					[id]: tags.filter((tag) => tag !== tagToRemove),
+				});
+			}}
+		/>;
+	},
+} as const;
+
+export function getFormControl(
+	field: FormControl,
+	newItem: Record<string, FormData>,
+	setNewItem: (newItem: Record<string, FormData>) => void,
+) {
+	switch (field.controlType) {
+		case "TextInput":
+			return controlTypes["TextInput"](
+				field, newItem, setNewItem,
+			);
+		case "TextArea":
+			return controlTypes["TextArea"](
+				field, newItem, setNewItem,
+			);
+		case "DropDown":
+			return controlTypes["DropDown"](
+				field, newItem, setNewItem,
+			);
+		case "Checkbox":
+			return controlTypes["Checkbox"](
+				field, newItem, setNewItem,
+			);
+		case "Toggle":
+			return controlTypes["Toggle"](
+				field, newItem, setNewItem,
+			);
+		case "TagManager":
+			return controlTypes["TagManager"](
+				field, newItem, setNewItem,
+			);
+	}
+}
+
+function isStringArray(values: unknown): values is string[] {
+	if (!Array.isArray(values)) return false;
+
+	return values.every((value) => typeof value === "string");
+}
