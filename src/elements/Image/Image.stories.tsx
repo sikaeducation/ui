4536166a4ef4cbb359c8ecaea010634b686ff272
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-02 16:39:41 -0600

    Add lightbox to image

--- src/elements/Image/Image.stories.tsx
+++ src/elements/Image/Image.stories.tsx
@@ -21,3 +21,37 @@ export const WithAttribution: Story = {
 		attribution: "Photo by Kyle Coberly",
 	},
 };
+
+export const WithLightBox: Story = {
+	args: {
+		src: "https://via.placeholder.com/500#medium",
+		alt: "Alt text",
+		attribution: "Photo by Kyle Coberly",
+		Lightbox: function Lightbox({ onClose, children }) {
+			return (
+				<div style={{
+					backgroundColor: "hsla(0, 0%, 0%, 0.5)",
+					height: "100%",
+					width: "100%",
+					position: "absolute",
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0,
+					zIndex: 2,
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}} onClick={() => onClose()}>
+					<div
+						style={{
+							zIndex: 3,
+						}}
+					>
+						{children}
+					</div>
+				</div>
+			);
+		},
+	},
+};
--- src/elements/Image/Image.tsx
+++ src/elements/Image/Image.tsx
@@ -1,20 +1,64 @@
+import classNames from "classnames";
+import { ComponentType, MouseEventHandler, ReactNode, useState } from "react";
 import "./Image.scss";
 
 type Props = {
 	src: string;
 	alt: string;
 	attribution?: string;
+	Lightbox?: ComponentType<{
+		children: ReactNode,
+		onClose: () => void;
+	}>;
 };
 
-export default function Image({ src, alt, attribution }: Props) {
+export default function Image({ src, alt, attribution, Lightbox }: Props) {
+	const [
+		isLightboxed,
+		setIsLightBoxed,
+	] = useState(false);
+
+	const potentialAttribution = attribution
+		&& <p className="attribution">{attribution}</p>;
+
+	const getImage = (clickHandler?: MouseEventHandler) => (
+		<img
+			onClick={clickHandler}
+			alt={alt}
+			src={src}
+		/>
+	);
+
+	const ImageElement = Lightbox && isLightboxed
+		? <Lightbox onClose={() => setIsLightBoxed(false)}>
+			<div className={classNames({
+				"image-wrapper": true,
+				"lightboxed": isLightboxed,
+			})}>
+				{
+					/* Swallow click, prevent bubbling */
+					getImage((event) => event.stopPropagation())
+				}
+				{potentialAttribution}
+			</div>
+		</Lightbox>
+		: <div className="image-wrapper">
+			{
+				Lightbox
+					? <>
+						{getImage(() => setIsLightBoxed(true))}
+						{potentialAttribution}
+					</>
+					: <>
+						{getImage()}
+						{potentialAttribution}
+					</>
+			}
+		</div>;
+
 	return (
 		<div className="Image">
-			<div className="image-wrapper">
-				<img alt={alt} src={src} />
-				{
-					attribution && <p className="attribution">Photo by Kyle Coberly</p>
-				}
-			</div>
+			{ImageElement}
 		</div>
 	);
 }
