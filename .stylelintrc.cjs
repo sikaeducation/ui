7a78a5aa820b9445e87d659a12c3c3464a615fe2
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-07 03:00:06 -0600

    Fixing formatting

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-	extends: ["@sikaeducation/stylelint-config"],
+  extends: ["@sikaeducation/stylelint-config"],
 };
--- src/components/Form/create-form-control-elements.tsx
+++ src/components/Form/create-form-control-elements.tsx
@@ -2,61 +2,61 @@ import { ReactNode } from "react";
 import { FormControl, getFormControl, NewFormData } from "./form-controls";
 
 function loadNextFormControl(
-	fields: FormControl[],
-	newItem: NewFormData,
-	setNewItem: (value: NewFormData) => void,
+  fields: FormControl[],
+  newItem: NewFormData,
+  setNewItem: (value: NewFormData) => void,
 ) {
-	const field = fields.shift()!;
-	return getFormControl(field, newItem, setNewItem);
+  const field = fields.shift()!;
+  return getFormControl(field, newItem, setNewItem);
 }
 
 // eslint-disable-next-line max-statements
 export default function createFormControlElements(
-	fields: FormControl[],
-	newItem: NewFormData,
-	setNewItem: (value: NewFormData) => void,
+  fields: FormControl[],
+  newItem: NewFormData,
+  setNewItem: (value: NewFormData) => void,
 ) {
-	let $fields: ReactNode[] = [];
+  let $fields: ReactNode[] = [];
 
-	while (fields.length > 0) {
-		const field = fields[0];
-		const formControl = loadNextFormControl(fields, newItem, setNewItem);
+  while (fields.length > 0) {
+    const field = fields[0];
+    const formControl = loadNextFormControl(fields, newItem, setNewItem);
 
-		const [nextField, thirdField] = fields;
-		if (
-			(field.width === "half" && nextField?.width === "half") ||
-			(field.width === "third" &&
-				nextField?.width === "third" &&
-				thirdField?.width !== "third")
-		) {
-			const nextFormControl = loadNextFormControl(fields, newItem, setNewItem);
-			$fields.push(
-				<div key={field.id} className="form-row">
-					<span className="form-field half">{formControl}</span>
-					<span className="form-field half">{nextFormControl}</span>
-				</div>,
-			);
-		} else if (
-			field.width === "third" &&
-			nextField?.width === "third" &&
-			thirdField?.width === "third"
-		) {
-			const nextFormControl = loadNextFormControl(fields, newItem, setNewItem);
-			const thirdFormControl = loadNextFormControl(fields, newItem, setNewItem);
-			$fields.push(
-				<div key={field.id} className="form-row">
-					<span className="form-field third">{formControl}</span>
-					<span className="form-field third">{nextFormControl}</span>
-					<span className="form-field third">{thirdFormControl}</span>
-				</div>,
-			);
-		} else {
-			$fields.push(
-				<div key={field.id} className="form-row form-field full">
-					{formControl}
-				</div>,
-			);
-		}
-	}
-	return $fields;
+    const [nextField, thirdField] = fields;
+    if (
+      (field.width === "half" && nextField?.width === "half") ||
+      (field.width === "third" &&
+        nextField?.width === "third" &&
+        thirdField?.width !== "third")
+    ) {
+      const nextFormControl = loadNextFormControl(fields, newItem, setNewItem);
+      $fields.push(
+        <div key={field.id} className="form-row">
+          <span className="form-field half">{formControl}</span>
+          <span className="form-field half">{nextFormControl}</span>
+        </div>,
+      );
+    } else if (
+      field.width === "third" &&
+      nextField?.width === "third" &&
+      thirdField?.width === "third"
+    ) {
+      const nextFormControl = loadNextFormControl(fields, newItem, setNewItem);
+      const thirdFormControl = loadNextFormControl(fields, newItem, setNewItem);
+      $fields.push(
+        <div key={field.id} className="form-row">
+          <span className="form-field third">{formControl}</span>
+          <span className="form-field third">{nextFormControl}</span>
+          <span className="form-field third">{thirdFormControl}</span>
+        </div>,
+      );
+    } else {
+      $fields.push(
+        <div key={field.id} className="form-row form-field full">
+          {formControl}
+        </div>,
+      );
+    }
+  }
+  return $fields;
 }
