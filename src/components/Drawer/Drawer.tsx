af647e967712480ff43c1a8b5dc538fe82f3814b
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-11 18:21:54 -0600

    Fix import path

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import { ReactNode } from "react";
-import Icon from "../Icon";
+import Icon from "../../elements/Icon";
 import "./Drawer.scss";
 
 type Props = {
