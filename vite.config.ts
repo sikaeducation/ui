2d7225536cf272de59631e9470ea29247086aeaf
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-17 04:43:32 -0600

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
-/** @type {import('vite').UserConfig} */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		dts({
-			rollupTypes: true,
-		}),
-	],
-	publicDir: "./style-library",
-	build: {
-		lib: {
-			entry: "src/index.ts",
-			formats: ["es"],
-			fileName: "components",
-		},
-		minify: false,
-		cssMinify: false,
-		sourcemap: true,
-		rollupOptions: {
-			external: ["react", "react-dom", "storybook"],
-		},
-	},
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+  publicDir: "./style-library",
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: "components",
+    },
+    minify: false,
+    cssMinify: false,
+    sourcemap: true,
+    rollupOptions: {
+      external: ["react", "react-dom", "storybook"],
+    },
+  },
 });
