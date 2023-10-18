ce958fa2c8092fa09e277c7308f9531bae34e856
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-18 02:42:49 -0600

    Rework scripts

--- static-server-test.sh
+++ static-server-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npx concurrently -k -s first -n "Storybook,Component Tests" -c "bgGray.white,auto" \
-	"npx http-server storybook-static --port 6006 --silent" \
-	"npx wait-on tcp:127.0.0.1:6006 && test-storybook --url http://127.0.0.1:6006"
+	"npx http-server storybook-static --port 7007 --silent" \
+	"npx wait-on tcp:127.0.0.1:7007 && test-storybook --url http://127.0.0.1:7007 -- --detectOpenHandles"
