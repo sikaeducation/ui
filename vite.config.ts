d408dc2665c06d7d5e5b8d1853a48dbaf7528be5
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-18 03:01:01 -0600

    Fix workflow file

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,24 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      rollupTypes: true,
-    }),
-  ],
-  publicDir: "./style-library",
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
-      fileName: "components",
-    },
-    minify: false,
-    cssMinify: false,
-    sourcemap: true,
-    rollupOptions: {
-      external: ["react", "react-dom", "storybook"],
-    },
-  },
+	plugins: [
+		react(),
+		dts({
+			rollupTypes: true,
+		}),
+	],
+	publicDir: "./style-library",
+	build: {
+		lib: {
+			entry: "src/index.ts",
+			formats: ["es"],
+			fileName: "components",
+		},
+		minify: false,
+		cssMinify: false,
+		sourcemap: true,
+		rollupOptions: {
+			external: ["react", "react-dom", "storybook"],
+		},
+	},
 });
