3fd2be23bb26f1e7ee4df393fb3da91d7478a47f
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-06 01:30:55 -0700

    Add SVG handling to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
+/// <reference types="vite-plugin-svgr/client" />
 /** @type {import('vite').UserConfig} */
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    svgr(),
     react(),
     dts({
       rollupTypes: true,
