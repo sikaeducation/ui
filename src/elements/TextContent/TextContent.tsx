79bf37e2fd3d1e484740c5e0de4a998bc886be7f
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-08 02:54:59 -0700

    Add display font

--- src/elements/TextContent/TextContent.tsx
+++ src/elements/TextContent/TextContent.tsx
@@ -5,14 +5,18 @@ import classNames from "classnames";
 type Props = {
   children: ReactNode;
   className?: string;
+  style: "body" | "hero";
 };
 
-export default function TextArea({ children, className = "" }: Props) {
+export default function TextArea(
+  { children, className = "", style = "body" }: Props,
+) {
   return (
     <p
       className={classNames({
         [className]: true,
         TextContent: true,
+        hero: style === "hero",
       })}
     >
       {children}
