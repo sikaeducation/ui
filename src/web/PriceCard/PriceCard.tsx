2f37cec96a8e1d52597c21ef9b3b3a5d4e48e528
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-13 22:16:22 -0700

    Add some boilerplate

--- src/web/PriceCard/PriceCard.tsx
+++ src/web/PriceCard/PriceCard.tsx
@@ -1,7 +1,18 @@
 import "./PriceCard.scss";
+import Button from "@/elements/Button";
 
 type Props = {};
 
 export default function PriceCard({}: Props) {
-  return <div className="PriceCard"></div>;
+  return (
+    <section className="PriceCard">
+      <header>
+        <h3>Programming Lessons</h3>
+      </header>
+      <div>
+        <p>Hey now</p>
+      </div>
+      <Button type="">Do it now</Button>
+    </section>
+  );
 }
