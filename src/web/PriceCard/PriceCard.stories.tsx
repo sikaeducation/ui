22d506aea383732112299a55163bd0195c6e5ee7
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-14 02:00:01 -0700

    Draft for PriceCard

--- src/web/PriceCard/PriceCard.stories.tsx
+++ src/web/PriceCard/PriceCard.stories.tsx
@@ -10,5 +10,38 @@ export default meta;
 type Story = StoryObj<typeof PriceCard>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    heading: "Some Heading",
+    subheading:
+      "Et dolore magna aliqua. Ut enim ad! Lorem ipsum dolor, sit amet.",
+    body: `
+Lorem ipsum dolor **sit amet**, consectetur adipiscing elit, sed do _eiusmod_ tempor incididunt ut labore
+
+Et dolore magna aliqua. Ut enim ad minim veniam, **quis nostrud exercitation** ullamco laboris nisi ut aliquip
+
+Ex ea commodo consequat. Duis aute **irure dolor in reprehenderit.**
+
+_In voluptate velit esse cillum dolore eu fugiat nulla pariatur_. Excepteur sint occaecat cupidatat **non proident**, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  `,
+    differentiator: "All the things",
+    price: {
+      sale: "$99",
+      full: "$199",
+      unit: "per lesson",
+    },
+    button: {
+      action: () => { },
+      text: "Take Action",
+    },
+  },
+};
+
+export const NoSale: Story = {
+  args: {
+    ...Default.args,
+    price: {
+      sale: "$99",
+      unit: "per lesson",
+    },
+  },
 };
--- src/web/PriceCard/PriceCard.tsx
+++ src/web/PriceCard/PriceCard.tsx
@@ -1,18 +1,42 @@
+import { Heading, Markdown, Tag, TextContent } from "@/index";
 import "./PriceCard.scss";
 import Button from "@/elements/Button";
 
-type Props = {};
+type Props = {
+  heading: string;
+  subheading: string;
+  differentiator: string;
+  body: string;
+  price: {
+    sale: string;
+    full?: string;
+    unit?: string;
+  };
+  button: {
+    action: () => void;
+    text: string;
+  };
+};
 
-export default function PriceCard({}: Props) {
+export default function PriceCard(
+  { heading, subheading, body, button, price, differentiator }: Props,
+) {
   return (
     <section className="PriceCard">
       <header>
-        <h3>Programming Lessons</h3>
+        <span className="badge">{differentiator}</span>
+        <Heading level={2}>{heading}</Heading>
+        <TextContent className="subheading">{subheading}</TextContent>
+        <div className="pricing">
+          <span className="sale">{price.sale}</span>
+          {price.full && <del>{price.full}</del>}
+          <span className="unit">{price.unit}</span>
+        </div>
       </header>
-      <div>
-        <p>Hey now</p>
+      <div className="content">
+        <Markdown content={body.trim()} />
+        <Button type="primary" action={button.action}>{button.text}</Button>
       </div>
-      <Button type="">Do it now</Button>
     </section>
   );
 }
