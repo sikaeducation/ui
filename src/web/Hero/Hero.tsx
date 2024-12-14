511570ee57b893a23fd2145831401d462df6073f
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-14 02:07:55 -0700

    Add Hero

---
+++ src/web/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import "./Hero.scss";
+
+type Props = {};
+
+export default function Hero({ }: Props) {
+  return (
+    <div className="Hero">
+      <p className="tagline">Tagline goes here</p>
+    </div>
+  );
+}
