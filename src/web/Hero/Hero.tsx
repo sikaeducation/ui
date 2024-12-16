cdb4fdd897d0db25a53d903e8ffff182a9261b68
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-15 21:38:18 -0700

    WIP Hero

--- src/web/Hero/Hero.tsx
+++ src/web/Hero/Hero.tsx
@@ -6,6 +6,7 @@ export default function Hero({ }: Props) {
   return (
     <div className="Hero">
       <p className="tagline">Tagline goes here</p>
+      <p className="subheading">Do the thing that helps you do the thing</p>
     </div>
   );
 }
