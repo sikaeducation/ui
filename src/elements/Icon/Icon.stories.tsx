ee4c827ed78520ba786b6cfbf0925ab185726cb6
Kyle Coberly <kyle.coberly@gmail.com>
2024-12-21 10:55:28 -0700

    Add Icon Demo

--- src/elements/Icon/Icon.stories.tsx
+++ src/elements/Icon/Icon.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryObj, Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import Icon from ".";
 
@@ -9,81 +9,6 @@ export default meta;
 
 type Story = StoryObj<typeof Icon>;
 
-export const Checkmark: Story = {
-  args: {
-    type: "checkmark",
-  },
-};
-export const Article: Story = {
-  args: {
-    type: "article",
-  },
-};
-export const Close: Story = {
-  args: {
-    type: "close",
-  },
-};
-export const Accepted: Story = {
-  args: {
-    type: "accepted",
-  },
-};
-export const Pending: Story = {
-  args: {
-    type: "pending",
-  },
-};
-export const Rejected: Story = {
-  args: {
-    type: "rejected",
-  },
-};
-export const Clear: Story = {
-  args: {
-    type: "clear",
-  },
-};
-export const Confident: Story = {
-  args: {
-    type: "confident",
-  },
-};
-export const Unclear: Story = {
-  args: {
-    type: "unclear",
-  },
-};
-export const Deferred: Story = {
-  args: {
-    type: "deferred",
-  },
-};
-export const Plus: Story = {
-  args: {
-    type: "plus",
-  },
-};
-export const Search: Story = {
-  args: {
-    type: "search",
-  },
-};
-export const View: Story = {
-  args: {
-    type: "view",
-  },
-};
-export const NoView: Story = {
-  args: {
-    type: "no-view",
-  },
-};
-export const Expand: Story = {
-  args: {
-    type: "expand",
-  },
-};
 export const Interactive: Story = {
   args: {
     type: "close",
